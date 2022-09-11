ca86f94544fa1dfae02b007f2feb28659d643806
hstt-coder <eduardolopes19@outlook.com>
2022-09-11 15:29:53 -0300

    Lessons seed, fix: lessonsDone

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -49,4 +49,26 @@ VALUES (1, 1, '2020-07-14T13:00:00Z', null, true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update)
 VALUES (2, 1, '2020-07-14T23:30:00Z', null, true, false);
 
+INSERT INTO tb_lesson (title, position, section_id) 
+VALUES('Aula 1 do capítulo 1', 1, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) 
+VALUES (1, 'Material de apoio: ABC', 'https://www.youtube.com/watch?v=WzO5QlkjVLA');
+
+INSERT INTO tb_lesson (title, position, section_id) 
+VALUES('Aula 2 do capítulo 1', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri)
+ VALUES (2, 'Material de apoio: XPTO', 'https://www.youtube.com/watch?v=WzO5QlkjVLA');
+
+INSERT INTO tb_lesson (title, position, section_id) 
+VALUES('Aula 3 do capítulo 1', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) 
+VALUES (3, 'Material de apoio: XYZ', 'https://www.youtube.com/watch?v=WzO5QlkjVLA');
+
+INSERT INTO tb_lesson (title, position, section_id) 
+VALUES('Tarefa do capítulo 1', 4, 1);
+INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, due_Date)
+VALUES (4, 'Fazer um trabalho legal', 5, 4, 1.0, '2020-07-24T13:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES(1,1,1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES(2,1,1);
 
